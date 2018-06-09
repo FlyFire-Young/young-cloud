037213c48f90fcdc3fe9519fe9f0747979194880
FlyFire-Young <mr_liuy01@163.com>
2018-06-09 23:11:32 +0800

    eurake s/c

---
+++ eurakeclient/src/main/java/cn/young/ClientApplication.java
@@ -0,0 +1,32 @@
+package cn.young;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Auther: Young
+ * @Date: 2018-06-09 22:01
+ * @Description:
+ */
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class ClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class, args);
+    }
+
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/")
+    public String home() {
+        return "hello world from port " + port;
+    }
+}
---
+++ eurakeserver/src/main/java/cn/young/ServerApplication.java
@@ -0,0 +1,20 @@
+package cn.young;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Auther: Young
+ * @Date: 2018-06-09 22:01
+ * @Description:
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+}
